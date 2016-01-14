25ec1b6f35f4229cd042b7e55cd683199bb04ee9
joshuablackwood <blackwood@uchicago.edu>
2016-01-14 12:22:30 -0600

    Program to convert Fahrenheit to Celsius

---
+++ solution_8.py
@@ -0,0 +1,10 @@
+# solution_8.py
+#  A program to convert Fahrenheit to Celsius
+# by: Joshua Blackwood
+
+def main():
+  fahrenheit=eval(input("What is the Fahrenheit temperature?"))
+  celsius=(fahrenheit-32)*5/9
+  print("The temperature is", celsius, "degrees Celsius.")
+
+main()
